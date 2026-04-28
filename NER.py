dea7ce41b8acc8654002da228593e78c129ae7db
RajviDave <rajvidave22@gmail.com>
2026-04-28 21:25:53 +0530

    spacy

--- NER.py
+++ NER.py
@@ -2,11 +2,9 @@ import spacy
 
 nlp=spacy.load("en_core_web_lg")
 
-text="My name is rajvi dave and i am queen and winner of this world"
+text="SQL and Python are two rajvi's strong sense"
 
 doc=nlp(text)
 
-print("Token\t\tPOS Tag")
-print("-----------------------")
-for token in doc:
-    print(f"{token.text}\t\t{token.pos_}")
+for ent in doc.ents:
+    print(ent.text, ent.label_)
