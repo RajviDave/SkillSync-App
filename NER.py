035f6298d218ed64073d428abd2d305ab089f7d5
RajviDave <rajvidave22@gmail.com>
2026-05-02 21:06:24 +0530

    NER

--- NER.py
+++ NER.py
@@ -1,10 +1,19 @@
 import spacy
+from spacy.pipeline import EntityRuler
 
-nlp=spacy.load("en_core_web_lg")
+nlp = spacy.load("en_core_web_sm")
 
-text="SQL and Python are two rajvi's strong sense"
+ruler = nlp.add_pipe("entity_ruler", before="ner")
 
-doc=nlp(text)
+patterns = [
+    {"label": "SKILL", "pattern": "Python"},
+    {"label": "SKILL", "pattern": "Machine Learning"},
+    {"label": "SKILL", "pattern": "React"},
+]
+
+ruler.add_patterns(patterns)
+
+doc = nlp("Skilled in Python and Machine Learning")
 
 for ent in doc.ents:
     print(ent.text, ent.label_)
