f62f4f117273086e6601bd32c7c64caee542a9e5
RajviDave <rajvidave22@gmail.com>
2026-06-26 23:40:17 +0530

    git config

--- db_connection.py
+++ db_connection.py
@@ -2,3 +2,13 @@ from flask import Flask,jsonify,send_from_directory
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
+
+app = Flask(__name__)
+
+db_config = {
+    "dbname": "your_database_name",
+    "user": "your_postgres_user",
+    "password": "your_database_password",
+    "host": "localhost",
+    "port": "5432"
+}
